d3c0fd7ebe9139006dd8fd01f4cd8d97fb8763ec
Sobhan Bera <sobhanbera258@gmail.com>
2022-01-08 23:30:11 +0530

    feat: added logo of the website also added the constants required

---
+++ constants/index.js
@@ -0,0 +1,11 @@
+/**
+ * this file contains all the contant variables
+ * which will be required througout the website
+ */
+
+export const GOOGLE_COLORS = {
+    BLUE: '#4285F4',
+    RED: '#DB4437',
+    YELLOW: '#F4B400',
+    GREEN: '#0F9D58',
+}
