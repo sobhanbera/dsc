d0f38aa74c48e5d132a99429e1d8efa56162c5c7
Sobhan Bera <sobhanbera258@gmail.com>
2022-01-08 23:31:08 +0530

    initial header and footer component with linking of them

---
+++ components/Header/index.js
@@ -0,0 +1,6 @@
+export default function Header(props) {
+    return (
+        <div>Header</div>
+    )
+}
+
---
+++ components/index.js
@@ -0,0 +1,5 @@
+import AsciiArt from './Ascii'
+import Header from './Header'
+import Footer from './Footer'
+
+export {AsciiArt, Header, Footer}
