4ccc250f1a52746d9b3691de6498d4aa2d2b10f5
Sobhan Bera <sobhanbera258@gmail.com>
2022-01-11 17:50:59 +0530

    many style code formatted and search bar features reduced which were unnecessary

--- components/SearchBar/index.js
+++ components/SearchBar/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { AiOutlineSearch } from 'react-icons/ai'
+import {AiOutlineSearch} from 'react-icons/ai'
 
 import styles from '../../styles/components/SearchBar/index.module.css'
 
 // this react coponent renders a cool search bar
 // it takes search term as a prop with name search and setSearch prop to set the search term
-export default function SearchBar({ searchText, setSearch, search }) {
+export default function SearchBar({searchText, setSearch}) {
     return (
         <div className={styles.searchBarContainer}>
             <input id="search_bar" type="text" dataValue="asdf" placeholder="Search..." autoComplete="off" value={searchText} onChange={e => setSearch(e.target.value)} />
