964f0224fef1887d697b243bb1eab407daf8c303
Sobhan Bera <sobhanbera258@gmail.com>
2022-01-12 23:41:26 +0530

    removed unwanted code from main page

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,10 @@
 import {useEffect, useState} from 'react'
 import Head from 'next/head'
-import {useInView} from 'react-intersection-observer'
 
 import {GOOGLE_COLORS} from '../constants'
-import axios from 'axios'
-
-function ColorTrigger({changeColor, color}) {
-    const [ref, inView] = useInView()
-
-    useEffect(() => {
-        if (inView) {
-            console.log('Color changed to:', color)
-            changeColor(color)
-        }
-    }, [inView])
-
-    return (
-        <div
-            ref={ref}
-            style={{
-                height: '100vh',
-            }}></div>
-    )
-}
 
 export default function LandingPage() {
     const [backgroundColor, setBackgroundColor] = useState('#ffffff')
-
-    function changeColor(color = '#ffffff') {
-        // setBackgroundColor(color)
-    }
 
     useEffect(() => {
         window.addEventListener('scroll', () => {
@@ -65,23 +40,11 @@ export default function LandingPage() {
             <Head>
                 <title>Google Developer Student Clubs GHRCE</title>
             </Head>
-            <p>Color is white currently... keep scrolling...</p>
-            <ColorTrigger color={'#FFFFFF'} changeColor={changeColor} />
-
-            <p>Color will change here to blue</p>
-            <ColorTrigger color={GOOGLE_COLORS.BLUE + '50'} changeColor={changeColor} />
-
+            {/* testing purpose texts */}
+            {/* <p>Color is blue currently... keep scrolling...</p>
             <p>Color will change here to redish</p>
-            <ColorTrigger color={GOOGLE_COLORS.RED + '50'} changeColor={changeColor} />
-
             <p>Color will change here to green</p>
-            <ColorTrigger color={GOOGLE_COLORS.GREEN + '50'} changeColor={changeColor} />
-
-            <p>Color will change here to yellow</p>
-            <ColorTrigger color={GOOGLE_COLORS.YELLOW + '50'} changeColor={changeColor} />
-
-            <p>Color will now change to white back.</p>
-            <ColorTrigger color={'#ffffff'} changeColor={changeColor} />
+            <p>Color will change here to yellow</p> */}
         </div>
     )
 }
