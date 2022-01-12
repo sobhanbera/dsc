38cde29d1b9fce16a63f85d1498ed5b80d55d404
Sobhan Bera <sobhanbera258@gmail.com>
2022-01-13 00:32:36 +0530

    Textinput component extra features

--- components/TextInput/index.js
+++ components/TextInput/index.js
@@ -1,11 +1,6 @@
 import React from 'react'
 
-import styles from '../../styles/components/SearchBar/index.module.css'
-
-export default function TextInput({searchText, setSearch, placeholder}) {
-    return (
-        <div className={styles.searchBarContainer}>
-            <input id="search_bar" type="text" placeholder={placeholder || 'Search...'} autoComplete="off" value={searchText} onChange={e => setSearch(e.target.value)} />
-        </div>
-    )
+export default function TextInput(props) {
+    const {searchText, setSearch} = props
+    return <input id="search_bar" type="text" autoComplete="off" value={searchText} onChange={e => setSearch(e.target.value)} {...props} />
 }
