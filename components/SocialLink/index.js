5c734fbe7e44d94cf2a3b422842d8b9dcb3e6ef1
Sobhan Bera <sobhanbera258@gmail.com>
2022-01-13 17:44:47 +0530

    feat: links editing feature added

--- components/SocialLink/index.js
+++ components/SocialLink/index.js
@@ -3,11 +3,18 @@ import {AiOutlineLink} from 'react-icons/ai'
 
 import styles from '../../styles/components/SocialLink/index.module.css'
 import {isColorBright, shortenText} from '../../utils'
+import {SocialPlatformsIconsData} from '../../constants/socials'
 
-export default function AdminSocialLinkPreview({data, onEdit, onDelete}) {
+export default function SocialLink({data, onEdit, onDelete}) {
+    const IconData = SocialPlatformsIconsData.filter(iconData => iconData.tag === data.iconTag)[0]?.Icon
+
     return (
         <div className={styles.socialLinkCard}>
             <p>{data.tag}</p>
+            <div className={styles.socialLinkCardPlatformIcon}>
+                <IconData fill={data.color} />
+            </div>
+
             <p>
                 <a href={data.link} target="_blank" rel="noreferrer">
                     {shortenText(data?.link + '', 30)}
